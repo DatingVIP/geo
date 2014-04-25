d9ce28efda372887fa6d6e683a3da1bf39f0dd9a
Boris Momčilović <boris.momcilovic@gmail.com>
2014-04-25 16:31:50 +0200

    Postal Code validation added

---
+++ src/DatingVIP/geo/Validation/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Validation Exception
+ *
+ * @package	DatingVIP
+ * @subpackage geo
+ * @author Boris Momcilovic <boris@firstbeatmedia.com>
+ * @copyright &copy; 2014 firstbeatmedia.com
+ * @version 1.0
+ */
+
+namespace DatingVIP\geo;
+
+class Validation_Exception extends \Exception { }
